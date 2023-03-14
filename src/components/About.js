c50d2fb1fe2423eea6f9b69c2dd15a34628acf54
FrancisLao9 <lao.francis@gmail.com>
2023-03-14 14:53:11 -0400

    importing starting code (referencing Mini Project)

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <div>
+      <div className="pro-image">
+        <i className="fas fa-user-circle" style={{ fontSize: "100px" }}></i>
+      </div>
+      <p>
+      Hello! My name is Francis Lao. I have experience in HTML, CSS, and JavaScript. I am currently looking for a full stack development position. I am a Computer Science from New Jersey City University. I currently have both an Associate's and Bachelor's degree while currently enrolled in Rutgers Full Stack Coding Camp. I live in Jersey City, New Jersey. Outside of coding, I personally enjoy playing card games, write, and do arts and crafts.
+      </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
