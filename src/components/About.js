eccd6fbc5421248274ae3bdd57003cb7cb0ce7e6
FrancisLao9 <lao.francis@gmail.com>
2023-03-24 13:19:25 -0400

    starting code

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,10 @@ function About() {
         <i className="fas fa-user-circle" style={{ fontSize: "100px" }}></i>
       </div>
       <p>
-      Hello! My name is Francis Lao. I have experience in HTML, CSS, and JavaScript. I am currently looking for a full stack development position. I am a Computer Science from New Jersey City University. I currently have both an Associate's and Bachelor's degree while currently enrolled in Rutgers Full Stack Coding Camp. I live in Jersey City, New Jersey. Outside of coding, I personally enjoy playing card games, write, and do arts and crafts.
+      Hello! My name is Francis Lao. I was born in the United States when my parents decided to settle from the Philippines.
+      I am a Computer Science from New Jersey City University. 
+      I currently have both an Associate's and Bachelor's degree while currently enrolled in Rutgers Full Stack Coding Camp. 
+      I live in Jersey City, New Jersey. Outside of coding, I personally enjoy playing card games, write, and do arts and crafts.
       </p>
       </div>
     </section>
